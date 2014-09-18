ab22486b0214dca5e7efb2132ed5eeba874e80d9
Nattawut Phetmak <neizod@gmail.com>
2014-09-19 04:49:19 +0700

    init project

---
+++ app/src/main/java/com/neizod/rgbw/FullscreenActivity.java
@@ -0,0 +1,47 @@
+package com.neizod.rgbw;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+
+public class FullscreenActivity extends Activity {
+    private static final int AUTO_HIDE_DELAY_MILLIS = 2000;
+    private Handler mHideHandler = new Handler();
+    private Runnable mHideRunnable = new Runnable() {
+        @Override
+        public void run() {
+            hideSystemUi();
+        }
+    };
+
+    private void hideSystemUi() {
+        getWindow().getDecorView().setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_IMMERSIVE
+        );
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fullscreen);
+
+        hideSystemUi();
+        getWindow().getDecorView().setOnSystemUiVisibilityChangeListener(
+            new View.OnSystemUiVisibilityChangeListener() {
+                @Override
+                public void onSystemUiVisibilityChange(int visibility) {
+                    if (visibility == 0) {
+                        mHideHandler.postDelayed(mHideRunnable, AUTO_HIDE_DELAY_MILLIS);
+                    }
+                }
+            }
+        );
+    }
+}
