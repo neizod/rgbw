d1ad27a8ac1bf540955adaabc6b55b271792ef6d
Nattawut Phetmak <neizod@gmail.com>
2014-09-19 18:08:37 +0700

    [fx] show next background color when click

--- app/src/main/java/com/neizod/rgbw/FullscreenActivity.java
+++ app/src/main/java/com/neizod/rgbw/FullscreenActivity.java
@@ -1,13 +1,18 @@
 package com.neizod.rgbw;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.FrameLayout;
 
 
 public class FullscreenActivity extends Activity {
     private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
+    private static final int[] COLOR_LIST = {Color.BLACK, Color.RED, Color.GREEN, Color.BLUE, Color.WHITE};
+    private static int iColor = 0;
+    private FrameLayout mScreen;
     private Handler mHideHandler = new Handler();
     private Runnable mHideRunnable = new Runnable() {
         @Override
@@ -27,10 +32,18 @@ public class FullscreenActivity extends Activity {
         );
     }
 
+    private void nextBackgroundColor() {
+        iColor = (iColor + 1) % COLOR_LIST.length;
+        mScreen.setBackgroundColor(COLOR_LIST[iColor]);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fullscreen);
+
+        mScreen = (FrameLayout) findViewById(R.id.fullscreen_root);
+        mScreen.setBackgroundColor(COLOR_LIST[iColor]);
 
         hideSystemUi();
         getWindow().getDecorView().setOnSystemUiVisibilityChangeListener(
@@ -43,5 +56,12 @@ public class FullscreenActivity extends Activity {
                 }
             }
         );
+
+        mScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nextBackgroundColor();
+            }
+        });
     }
 }
